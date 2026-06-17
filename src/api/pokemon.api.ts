a2af6b3b8421f1cfc0d1214b1cec357495002653
Rodri <rodri@MacBook-Pro-de-Rodri.local>
2026-06-17 20:23:06 -0300

    refactor: support request cancellation in API services

--- src/api/pokemon.api.ts
+++ src/api/pokemon.api.ts
@@ -1,26 +1,33 @@
 
 import { PokemonDetailResponse, PokemonListResponse } from '@/types/pokemon.types';
 import { axiosClient } from './axiosClient';
+import { QueryFunctionContext } from '@tanstack/react-query';
 
 export const getPokemonList = async (
   offset: number,
-  limit: number
+  limit: number,
+  {signal}: QueryFunctionContext
 ): Promise<PokemonListResponse> => {
   const response = await axiosClient.get<PokemonListResponse>('/pokemon', {
     params: {
       offset,
       limit,
     },
+    signal
   });
 
   return response.data;
 };
 
 export const getPokemonDetail = async (
-  nameOrId: string
+  nameOrId: string,
+  {signal}: QueryFunctionContext
 ): Promise<PokemonDetailResponse> => {
   const response = await axiosClient.get<PokemonDetailResponse>(
-    `/pokemon/${nameOrId}`
+    `/pokemon/${nameOrId}`,
+    {
+      signal
+    }
   );
 
   return response.data;
