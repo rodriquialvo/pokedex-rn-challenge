ef841bb05ac2a41187052e50fe172109b3f9d4a2
Rodri <rodri@MacBook-Pro-de-Rodri.local>
2026-06-18 21:07:15 -0300

    ..

--- App.tsx
+++ App.tsx
@@ -18,7 +18,7 @@ export default function App() {
       await SplashScreen.hideAsync();
     };
 
-    void prepare();
+    prepare();
 
     const iconTimeout = setTimeout(() => {
       setLaunchStage("splash");
